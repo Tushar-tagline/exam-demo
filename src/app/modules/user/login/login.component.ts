f845fabc4280d5ec474f4a193b3c22258b1cb910
tushar-tagline <tushar.tagline@gmail.com>
2022-02-18 10:49:16 +0530

    data store in localstorage

--- src/app/modules/user/login/login.component.ts
+++ src/app/modules/user/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { UserserviesService } from 'src/app/userservies.service';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,20 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  password: any;
+  email: any;
   
-  constructor() { }
+  constructor(private userService:UserserviesService) { }
 
   ngOnInit(): void {
   }
-  
+  loginSubmit() {
+    var data = {
+      email:this.email,
+      password:this.password
+    }
+    this.userService.signIn(data).subscribe((res:any) => {
+      localStorage.setItem('user', JSON.stringify(res.data))
+    })
+  }
 }
