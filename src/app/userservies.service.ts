3d00df16d3377e00677d22fb57402eefe73ae678
tushar-tagline <tushar.tagline@gmail.com>
2022-02-18 10:27:36 +0530

    post api in signup and login form

--- src/app/userservies.service.ts
+++ src/app/userservies.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { loginInter, Userinterface } from './userinterface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserserviesService {
 
-  constructor() { }
+  private url = environment.url;
+  constructor( private http:HttpClient) { }
+  ngOnInit(): void {}
+
+  public signUp(data: Userinterface): Observable<any> {
+    return this.http.post(this.url + 'users/signUp', data);
+  }
+  public signIn(data:loginInter): Observable<any> {
+    return this.http.post(this.url + 'users/Login', data);
+  }
 }
