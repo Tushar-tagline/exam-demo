0da15cb929735e9f8a5b8b763410ec4d3934a24d
tushar-tagline <tushar.tagline@gmail.com>
2022-02-18 09:59:40 +0530

    validation complete in sign-up form

--- src/app/modules/user/sign-up/sign-up.component.ts
+++ src/app/modules/user/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,18 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
-  public role:Array<any>=[{
-    id:'1',
-    value:'Techer'
+  public signup!: FormGroup
+  public role: Array<any> = [{
+    id: '1',
+    value: 'Techer'
   },
-    {
-      id:'2',
-      value:'Student'
-    }
+  {
+    id: '2',
+    value: 'Student'
+  }
   ]
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.signup = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-z]).{6,32}$'),
+      ]),
 
+      selected: new FormControl(null, Validators.required)
+    });
+  }
+  onsumbit() {
+    console.log(this.signup)
+  }
+  // get name() {
+  //   return this.signup.get('name')
+  // }
+  // get email() {
+  //   return this.signup.get('email')
+  // }
+  // get password() {
+  //   return this.signup.get('password')
+  // }
+  // get roles() {
+  //   return this.signup.get('selected')
+  // }
 }
