baf6c65cdedd52da6aec9d5e4ed440dff3f9006d
tushar-tagline <tushar.tagline@gmail.com>
2022-02-18 10:08:26 +0530

    create interface login

--- src/app/userinterface.ts
+++ src/app/userinterface.ts
@@ -1,2 +1,14 @@
 export interface Userinterface {
+    name:string,
+    email:string,
+    password:string,
+    role:Role
+}
+export interface Role {
+    teacher:string,
+    student:string
+}
+export interface loginInter{
+    email:string,
+    password:string
 }
