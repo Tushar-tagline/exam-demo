31e6511fe9e5fbd1de36f30825a1703ac45deb10
tushar-tagline <tushar.tagline@gmail.com>
2022-02-23 18:34:43 +0530

    add toster in view-exampage

--- src/app/modules/teacher/view-exampage/view-exampage.component.ts
+++ src/app/modules/teacher/view-exampage/view-exampage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgToastService } from 'ng-angular-popup';
 import { viewExam } from 'src/app/interface/userinterface';
 import { UserserviesService } from 'src/app/servies/userservies.service';
 import { ViewexamdetailComponent } from '../viewexamdetail/viewexamdetail.component';
@@ -11,23 +12,30 @@ import { ViewexamdetailComponent } from '../viewexamdetail/viewexamdetail.compon
 })
 export class ViewExampageComponent implements OnInit {
   public viewexams = [
-    {notes:'', _id:'', subjectName:'', email:'', _v:''}
+    { notes: '', _id: '', subjectName: '', email: '', _v: '' }
   ];
   public datalode: boolean = true;
-  constructor(private userService: UserserviesService,public modelservies: NgbModal) { }
+  constructor(private userService: UserserviesService, public modelservies: NgbModal, private toster: NgToastService) { }
 
   ngOnInit(): void {
-    this.userService.viewexam().subscribe((data:any)=>{console.log(data)
-      this.viewexams=data.data;
-      this.datalode=false;
+    this.userService.viewexam().subscribe((data: any) => {
+      console.log(data)
+      this.viewexams = data.data;
+      this.datalode = false;
+      if (data.statuscode === 200) {
+        this.toster.success({ detail: "View Exam-page successfully", summary: "View Exam-page  successfully", duration: 4000 })
+      }
+      else {
+        this.toster.error({ detail: "error message", summary: "View Exam-page  is failed", duration: 4000 })
+      }
     })
-    
+
   }
-  particularexam(id:any){
-    this.userService.particularexam(id).subscribe((res:any) => {
+  particularexam(id: any) {
+    this.userService.particularexam(id).subscribe((res: any) => {
       const modelRef = this.modelservies.open(ViewexamdetailComponent);
-       modelRef.componentInstance.que=res?.data.questions;
-      
+      modelRef.componentInstance.que = res?.data.questions;
+
     })
   }
 
