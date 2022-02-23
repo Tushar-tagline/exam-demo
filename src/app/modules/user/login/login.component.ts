3ba687b85fddc6723fc13d0841d0184b95d1af02
tushar-tagline <tushar.tagline@gmail.com>
2022-02-23 18:32:23 +0530

    formate document

--- src/app/modules/user/login/login.component.ts
+++ src/app/modules/user/login/login.component.ts
@@ -12,34 +12,35 @@ import { UserserviesService } from 'src/app/servies/userservies.service';
 })
 export class LoginComponent implements OnInit {
   public token!: any;
-  
 
-  constructor(private userService: UserserviesService, private toster: NgToastService,private route:Router) { }
+  constructor(private userService: UserserviesService, private toster: NgToastService, private route: Router) { }
 
   ngOnInit(): void {
 
   }
-loginSubmit(event: any) {
+  loginSubmit(event: any) {
     console.log('event :>> ', event.value);
     this.userService.signIn(event.value).subscribe((res: any) => {
       console.log('res.data.token :>> ', res.data.token);
       if (res.statusCode === 200 && res.data.role === 'teacher') {
-        this.token=localStorage.setItem('token',res.data.token);
+        this.token = localStorage.setItem('token', res.data.token);
         this.route.navigate(['teacher/dashboard']);
         this.toster.success({ detail: "successfully message", summary: "successfully signin", duration: 4000 })
       }
       else if (res.statusCode === 200 && res.data.role === 'student') {
-        this.token=localStorage.setItem('token',res.data.token);
+        this.token = localStorage.setItem('token', res.data.token);
         this.route.navigate(['student/dashboard']);
         this.toster.success({ detail: "successfully message", summary: "successfully signin", duration: 4000 })
       }
       else {
         this.toster.error({ detail: "error message", summary: "signin is failed", duration: 4000 })
       }
-      
+
+
     });
 
   }
+
 }
 
 
