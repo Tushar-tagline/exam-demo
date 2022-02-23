37e5cb476a3ca54ca4f3923970622569d57df792
tushar-tagline <tushar.tagline@gmail.com>
2022-02-23 18:39:40 +0530

    show the data in profile cpmponect

--- src/app/modules/student/profile/profile.component.ts
+++ src/app/modules/student/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Profiles } from 'src/app/interface/userinterface';
+import { UserserviesService } from 'src/app/servies/userservies.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  public studentprofile=[{
+    email:'',
+    name:'',
+    role:'',
+    _id:''
+  }]
+  
+  // public datalode: boolean = false;
+  constructor(private userService: UserserviesService) { }
 
   ngOnInit(): void {
+    this.userService.viewprofile().subscribe((data:any) => { console.log(data)
+    this.studentprofile=data.data
+    console.log('this.studentprofile :>> ', this.studentprofile);
+    // this.datalode=true;
+    })
   }
 
 }
+
