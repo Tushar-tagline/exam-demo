86e973c969c204d02540b9ba2afc9b40c827b4d6
tushar-tagline <tushar.tagline@gmail.com>
2022-02-24 15:29:01 +0530

    change all interface name

--- src/app/shared/servies/userservies.service.ts
+++ src/app/shared/servies/userservies.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Examlist, loginInter, Profiles, Studentlist, Userinterface, viewExam } from '../interface/userinterface';
+import { IExamlist, Ilogin, IProfiles, IStudentlist, IUser, IviewExam } from '../interface/userinterface';
 
 @Injectable({
   providedIn: 'root'
@@ -16,59 +16,59 @@ export class UserserviesService {
 
   ngOnInit(): void { }
 
-  public signUp(data: Userinterface): Observable<Userinterface> {
-    return this.http.post<Userinterface>(this.url + 'users/signUp', data);
+  public signUp(data: IUser): Observable<IUser> {
+    return this.http.post<IUser>(this.url + 'users/signUp', data);
   }
-  public signIn(data: any): Observable<loginInter> {
-    return this.http.post<loginInter>(this.url + 'users/Login' , data);
+  public signIn(data: any): Observable<Ilogin> {
+    return this.http.post<Ilogin>(this.url + 'users/Login' , data);
   }
-  public studentlist():Observable <Studentlist>{
+  public studentlist():Observable <IStudentlist>{
     let headers = new HttpHeaders()
      headers=headers.set('content-type','application/json')
      headers=headers.set('Access-Control-Allow-Origin', '*');
      headers=headers.set('access-token', `${this.getToken}`);
-     return this.http.get<Studentlist>(this.url + 'dashboard/Teachers',{headers:headers})
+     return this.http.get<IStudentlist>(this.url + 'dashboard/Teachers',{headers:headers})
    }
-   public particularstudentdata(studentid:any):Observable <Studentlist>{
+   public particularstudentdata(studentid:any):Observable <IStudentlist>{
     let headers = new HttpHeaders()
      headers=headers.set('content-type','application/json')
      headers=headers.set('Access-Control-Allow-Origin', '*');
      headers=headers.set('access-token', `${this.getToken}`);
-     return this.http.get<Studentlist>(this.url + 'dashboard/Teachers/viewStudentDetail?id='+studentid,{headers:headers})
+     return this.http.get<IStudentlist>(this.url + 'dashboard/Teachers/viewStudentDetail?id='+studentid,{headers:headers})
    }
-   public viewexam():Observable <viewExam>{
+   public viewexam():Observable <IviewExam>{
     let headers = new HttpHeaders()
      headers=headers.set('content-type','application/json')
      headers=headers.set('Access-Control-Allow-Origin', '*');
      headers=headers.set('access-token', `${this.getToken}`);
-     return this.http.get<viewExam>(this.url + 'dashboard/Teachers/viewExam',{headers:headers})
+     return this.http.get<IviewExam>(this.url + 'dashboard/Teachers/viewExam',{headers:headers})
    }   
    public particularexam(examid:any){
     let headers = new HttpHeaders()
     headers=headers.set('content-type','application/json')
     headers=headers.set('Access-Control-Allow-Origin', '*');
     headers=headers.set('access-token', `${this.getToken}`);
-    return this.http.get<viewExam>(this.url + 'dashboard/Teachers/examDetail?id='+examid,{headers:headers})
+    return this.http.get<IviewExam>(this.url + 'dashboard/Teachers/examDetail?id='+examid,{headers:headers})
    }
-   public viewprofile():Observable<Profiles>{
+   public viewprofile():Observable<IProfiles>{
     let headers = new HttpHeaders()
     headers=headers.set('content-type','application/json')
     headers=headers.set('Access-Control-Allow-Origin', '*');
     headers=headers.set('access-token', `${this.getToken}`);
-    return this.http.get<Profiles>(this.url +'student/getStudentDetail',{headers:headers})
+    return this.http.get<IProfiles>(this.url +'student/getStudentDetail',{headers:headers})
    }
-   public examlist():Observable<Examlist>{
+   public examlist():Observable<IExamlist>{
     let headers = new HttpHeaders()
     headers=headers.set('content-type','application/json')
     headers=headers.set('Access-Control-Allow-Origin', '*');
     headers=headers.set('access-token', `${this.getToken}`);
-    return this.http.get<Examlist>(this.url +'student/studentExam',{headers:headers})
+    return this.http.get<IExamlist>(this.url +'student/studentExam',{headers:headers})
    }
-   public stuexamlist(stuexamid:any):Observable<any>{
+   public stuexamlist(id:any):Observable<any>{
      let headers = new HttpHeaders()
      headers=headers.set('content-type','application/json');
      headers=headers.set('Access-Control-Allow-origin','*');
      headers=headers.set('access-token',`${this.getToken}`);
-     return this.http.get<any>(this.url+'/student/examPaper?id='+stuexamid,{headers:headers})
+     return this.http.get<any>(this.url+'student/examPaper?'+ 'id=' + id,{headers:headers})
    }
 }
