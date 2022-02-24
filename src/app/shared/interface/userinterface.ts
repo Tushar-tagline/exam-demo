774ba488658b7a9a4cdedefb95b379b602319995
tushar-tagline <tushar.tagline@gmail.com>
2022-02-24 18:30:00 +0530

    create all interface responce

--- src/app/shared/interface/userinterface.ts
+++ src/app/shared/interface/userinterface.ts
@@ -1,60 +1,94 @@
-import { Token } from "@angular/compiler"
-import { LoginComponent } from "src/app/modules/user/login/login.component"
-
+//IUser is SignUp interface
 export interface IUser {
-    name:string,
-    email:string,
-    password:string,
-    role:IRole
+    name: string,
+    email: string,
+    password: string,
+    role: IRole
+}
+export interface ISignUpRes {
+    message: string,
+    statusCode: number,
+    data: IUser[]
 }
 export interface IRole {
-    teacher:string,
-    student:string
+    teacher: string,
+    student: string
 }
-export interface Ilogin{
-    email:string,
-    password:string
-    token:string,
-    role:string
+//ILogin is Login interface 
+export interface Ilogin {
+    email: string,
+    password: string
+    token: string,
+    role: string
 }
-export interface IStudentlist{
-    status:string,
-    _id:number,
-    name:string,
-    email:string
+export interface IloginRes {
+    message: string,
+    statusCode: number,
+    data: Ilogin[]
 }
-export interface IviewExam{
-    _id:string,
-    subjectName:string,
-    email:string,
-    notes:string
+// IStudentList is studentlist interface
+export interface IStudentlist {
+    status: string,
+    _id: number,
+    name: string,
+    email: string
 }
-export interface IProfiles{
-    _id:string,
-    name:string,
-    email:string,
-    role:IRole
+export interface IStudentlistRes {
+    message: string,
+    statusCode: number,
+    data: IStudentlist[]
+    count: number
 }
-export interface IExamlist{
-    _id:string,
-    subjectName:string,
-    email:string,
-    notes:string
-    Result:string
+//IViewExam is viewexampage interface
+export interface IviewExam {
+    _id: string,
+    subjectName: string,
+    email: string,
+    notes: string
 }
-// export interface IviewExamPage{
-//     data:Array<string>,
-//     message:string,
-//     statusCode:number,
-//     userId:string
-// }
-export interface Iloginres{
-    message:string,
-    statusCode:number,
-    data:Ilogin[]
+export interface IviewExamPageRes {
+    message: string,
+    statusCode: number,
+    data: IviewExam[]
 }
-export interface Iexamdetail{
-    question:string,
-    options:string;
-    _id:string
+// IexamList is student exam-list interface
+export interface IExamlist {
+    _id: string,
+    subjectName: string,
+    email: string,
+    notes: string
+    Result: string
 }
+export interface IExamlistRes {
+    message: string,
+    statusCode: number,
+    userId: string,
+    data: IExamlist[]
+}
+// Iprofiles is profile interface
+export interface IProfiles {
+    _id: string,
+    name: string,
+    email: string,
+    role: IRole
+}
+export interface IProfileRes {
+    message: string,
+    statusCode: number,
+    data: IProfiles[]
+}
+//ExamDetail is student View exam-page interface
+export interface IExamDetail {
+    question: string,
+    options: string;
+    _id: string
+}
+export interface IExamPaper {
+    message: string,
+    statusCode: number,
+    data: IExamDetail[]
+}
+
+
+
+
